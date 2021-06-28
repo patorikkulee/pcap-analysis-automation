477a31e9cc4ba6d40bf1cdf1781bcc815c3daa06
sexypatrick <44813044+sexypatrick@users.noreply.github.com>
2021-06-28 23:30:36 +0800

    Add files via upload

---
+++ extract_payload.py
@@ -0,0 +1,54 @@
+from os import listdir
+from os.path import isfile, join
+import pandas
+
+protocol = str(input('protocol:'))
+dirpath = '/home/patrick/Downloads/' + protocol
+files = [f for f in listdir(dirpath) if isfile(join(dirpath, f)) and '.txt' in f and 'metadata' not in f] # get all files in a directory
+df = pandas.DataFrame(columns=['Attack pattern ID', 'subcluster ID', 'isp', 'payload']) # create a new dfto store
+
+clusterSeparator = '==============================\n'
+payloadSeparator = '----------------------------\n'
+
+# main function of parsing a file
+def main(filename):
+    parsed = [] # store parsed data
+
+    with open(dirpath + '/' + filename, 'r', encoding='UTF8') as f:
+        subclusters = [l.split(',') for l in ','.join(f.readlines()).split(clusterSeparator) if l.split(',') != ['']] # separate subclusters in file
+        
+        # remove empty strings from separation process
+        for sub in subclusters:
+            while '' in sub:
+                sub.remove('')
+        
+        for sub in subclusters:
+            subcluster = [l.split(',') for l in ','.join(sub).split(payloadSeparator) if l.split(',') != ['']] # separate payload in subclusters
+            
+            # remove empty strings from separation process
+            for payload in subcluster: 
+                while '' in payload:
+                    payload.remove('')
+            
+            parsed.append(subcluster)
+    
+    global df
+    attackpatternid = filename.rstrip('.txt')
+    for sub in parsed:
+        subclusterid = sub[0][0].lstrip('id: ').rstrip('\n')
+        isp = sub[0][1].lstrip('isp: ').rstrip('\n')
+        
+        for session in sub:
+            if 'Attacker to Honeypot\n' in session:
+                position = session.index('Attacker to Honeypot\n')
+                payload = ''.join(session[position+1:]).lstrip('\n').rstrip('\n')
+                
+                row = {'Attack pattern ID': attackpatternid,'subcluster ID': subclusterid, 'isp': isp, 'payload': payload}
+                df = df.append(row, ignore_index=True)
+
+if __name__=='__main__':
+    for each in files:
+        main(each)
+
+    print('df size : ', df.shape)
+    df.to_csv(f'payload_{protocol}.csv', index=False) # save .csv file
